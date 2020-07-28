b653e5c916518c4900df25dd2d1c399479adfd35
Brian Mandel <bmandel@blackducksoftware.com>
2020-07-28 15:50:57 -0400

    Added classes from Polaris Jira-Service to allow a full OAuth dance with a Jira cloud server. Added a dependency to use googles oauth API

---
+++ src/main/java/com/synopsys/integration/jira/common/rest/oauth/JiraOAuthService.java
@@ -0,0 +1,121 @@
+/**
+ * int-jira-common
+ *
+ * Copyright (c) 2020 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.synopsys.integration.jira.common.rest.oauth;
+
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.function.Supplier;
+
+import org.apache.commons.codec.binary.Base64;
+
+import com.google.api.client.auth.oauth.AbstractOAuthGetToken;
+import com.google.api.client.auth.oauth.OAuthAuthorizeTemporaryTokenUrl;
+import com.google.api.client.auth.oauth.OAuthCredentialsResponse;
+import com.google.api.client.auth.oauth.OAuthParameters;
+import com.google.api.client.auth.oauth.OAuthRsaSigner;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.synopsys.integration.jira.common.model.oauth.OAuthAuthorizationData;
+import com.synopsys.integration.jira.common.model.oauth.OAuthCredentialsData;
+
+public class JiraOAuthService {
+    public static final String OUT_OF_BAND_TO_SHOW_TOKEN_SECRET = "oob";
+    public static final String TEMPORARY_TOKEN_SUFFIX = "/plugins/servlet/oauth/request-token";
+    public static final String AUTHORIZATION_URL_SUFFIX = "/plugins/servlet/oauth/authorize";
+    public static final String ACCESS_TOKEN_SUFFIX = "/plugins/servlet/oauth/access-token";
+
+    private final String jiraServerUrl;
+    private final KeyFactory rsaKeyFactory;
+
+    public JiraOAuthService(String jiraServerUrl, KeyFactory rsaKeyFactory) {
+        this.jiraServerUrl = jiraServerUrl;
+        this.rsaKeyFactory = rsaKeyFactory;
+    }
+
+    public OAuthParameters createOAuthParameters(OAuthCredentialsData oAuthCredentialsData) throws InvalidKeySpecException {
+        String accessTokenRequestUrl = jiraServerUrl + JiraOAuthService.ACCESS_TOKEN_SUFFIX;
+        JiraOAuthGetAccessToken getAccessToken = new JiraOAuthGetAccessToken(accessTokenRequestUrl);
+        getAccessToken.consumerKey = oAuthCredentialsData.getConsumerKey();
+        getAccessToken.signer = getOAuthRsaSigner(oAuthCredentialsData.getPrivateKey());
+        getAccessToken.temporaryToken = oAuthCredentialsData.getAccessToken();
+
+        return getAccessToken.createParameters();
+    }
+
+    public OAuthAuthorizationData createOAuthAuthorizationData(String consumerKey, String privateKey) throws InvalidKeySpecException, IOException {
+        JiraOAuthGetTemporaryToken getTemporaryToken = createJiraOAuthGetTemporaryToken(consumerKey, privateKey);
+        OAuthCredentialsResponse oAuthCredentialsResponse = getTemporaryToken.execute();
+
+        String authorizationToken = oAuthCredentialsResponse.token;
+        String authorizationUrl = getAuthorizationUrl(oAuthCredentialsResponse);
+
+        return new OAuthAuthorizationData(authorizationUrl, authorizationToken);
+    }
+
+    public JiraOAuthGetTemporaryToken createJiraOAuthGetTemporaryToken(String consumerKey, String privateKey) throws InvalidKeySpecException {
+        String temporaryTokenRequestUrl = jiraServerUrl + JiraOAuthService.TEMPORARY_TOKEN_SUFFIX;
+        JiraOAuthGetTemporaryToken getTemporaryToken = createTokenWithKeys(() -> new JiraOAuthGetTemporaryToken(temporaryTokenRequestUrl), consumerKey, privateKey);
+        getTemporaryToken.transport = new NetHttpTransport();
+        getTemporaryToken.callback = JiraOAuthService.OUT_OF_BAND_TO_SHOW_TOKEN_SECRET;
+
+        return getTemporaryToken;
+    }
+
+    public String getAuthorizationUrl(OAuthCredentialsResponse oAuthCredentialsResponse) {
+        String authorizeTemporaryTokenUrl = jiraServerUrl + JiraOAuthService.AUTHORIZATION_URL_SUFFIX;
+        OAuthAuthorizeTemporaryTokenUrl authorizationUrl = new OAuthAuthorizeTemporaryTokenUrl(authorizeTemporaryTokenUrl);
+        authorizationUrl.temporaryToken = oAuthCredentialsResponse.token;
+
+        return authorizationUrl.toString();
+    }
+
+    public JiraOAuthGetAccessToken getJiraOAuthGetAccessToken(String temporaryToken, String verificationCodeFromJira, String consumerKey, String privateKey) throws InvalidKeySpecException {
+        String accessTokenRequestUrl = jiraServerUrl + JiraOAuthService.ACCESS_TOKEN_SUFFIX;
+        JiraOAuthGetAccessToken getAccessToken = createTokenWithKeys(() -> new JiraOAuthGetAccessToken(accessTokenRequestUrl), consumerKey, privateKey);
+        getAccessToken.transport = new NetHttpTransport();
+        getAccessToken.verifier = verificationCodeFromJira;
+        getAccessToken.temporaryToken = temporaryToken;
+
+        return getAccessToken;
+    }
+
+    private <T extends AbstractOAuthGetToken> T createTokenWithKeys(Supplier<T> supplier, String consumerKey, String privateKey) throws InvalidKeySpecException {
+        T oAuthGetToken = supplier.get();
+        oAuthGetToken.consumerKey = consumerKey;
+        oAuthGetToken.signer = getOAuthRsaSigner(privateKey);
+        return oAuthGetToken;
+    }
+
+    private OAuthRsaSigner getOAuthRsaSigner(String privateKeyString) throws InvalidKeySpecException {
+        byte[] privateKeyBytes = Base64.decodeBase64(privateKeyString);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        PrivateKey privateKey = rsaKeyFactory.generatePrivate(keySpec);
+
+        OAuthRsaSigner oAuthRsaSigner = new OAuthRsaSigner();
+        oAuthRsaSigner.privateKey = privateKey;
+        return oAuthRsaSigner;
+    }
+
+}
---
+++ src/main/java/com/synopsys/integration/jira/common/rest/oauth/http/JiraHttpService.java
@@ -0,0 +1,120 @@
+/**
+ * int-jira-common
+ *
+ * Copyright (c) 2020 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.synopsys.integration.jira.common.rest.oauth.http;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpContent;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.gson.Gson;
+import com.synopsys.integration.exception.IntegrationException;
+
+public class JiraHttpService {
+    private final String baseUrl;
+    private final HttpRequestFactory httpRequestFactory;
+    private final Gson gson;
+
+    public JiraHttpService(String baseUrl, HttpRequestFactory httpRequestFactory, Gson gson) {
+        this.baseUrl = baseUrl;
+        this.httpRequestFactory = httpRequestFactory;
+        this.gson = gson;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public HttpResponse getResponse(String urlEndpoint) throws IOException {
+        GenericUrl url = new GenericUrl(baseUrl + urlEndpoint);
+        HttpRequest request = httpRequestFactory.buildGetRequest(url);
+        return request.execute();
+    }
+
+    public <T> T get(String urlEndpoint, Type responseType) throws IntegrationException {
+        HttpResponse httpResponse = null;
+        try {
+            httpResponse = getResponse(urlEndpoint);
+            if (!httpResponse.isSuccessStatusCode()) {
+                throw new IntegrationException(httpResponse.getStatusMessage());
+            }
+            return parseResponse(httpResponse, responseType);
+        } catch (IOException e) {
+            throw new IntegrationException(e.getMessage());
+        } finally {
+            disconnectResponse(httpResponse);
+        }
+    }
+
+    public HttpResponse post(String urlEndpoint, Object requestBodyObject) throws IOException {
+        GenericUrl url = new GenericUrl(baseUrl + urlEndpoint);
+        HttpRequest postRequest = buildPostRequest(url, requestBodyObject);
+        return postRequest.execute();
+    }
+
+    public <T> T post(String urlEndpoint, Object requestBodyObject, Class<T> responseClass) throws IntegrationException {
+        HttpResponse httpResponse = null;
+        try {
+            httpResponse = post(urlEndpoint, requestBodyObject);
+            if (!httpResponse.isSuccessStatusCode()) {
+                throw new IntegrationException(httpResponse.getStatusMessage());
+            }
+            return parseResponse(httpResponse, responseClass);
+        } catch (IOException e) {
+            throw new IntegrationException(e.getMessage());
+        } finally {
+            disconnectResponse(httpResponse);
+        }
+    }
+
+    protected HttpRequest buildPostRequest(GenericUrl url, Object requestBodyObject) throws IOException {
+        HttpContent requestContent = buildPostRequestContent(requestBodyObject);
+        HttpRequest postRequest = httpRequestFactory.buildPostRequest(url, requestContent);
+        return postRequest;
+    }
+
+    protected HttpContent buildPostRequestContent(Object requestBodyObject) {
+        String objectJson = gson.toJson(requestBodyObject);
+        return ByteArrayContent.fromString(null, objectJson);
+    }
+
+    protected void disconnectResponse(HttpResponse response) throws IntegrationException {
+        if (response == null) {
+            return;
+        }
+        try {
+            response.disconnect();
+        } catch (IOException e) {
+            throw new IntegrationException(e.getMessage());
+        }
+    }
+
+    protected <T> T parseResponse(HttpResponse response, Type responseType) throws IOException {
+        String responseString = response.parseAsString();
+        return gson.fromJson(responseString, responseType);
+    }
+}
