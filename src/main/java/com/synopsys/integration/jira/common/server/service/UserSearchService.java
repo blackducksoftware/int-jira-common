15b7c4f7409be56d291a6957980395500a9a9129
Brian Mandel <bmandel@blackducksoftware.com>
2020-09-21 15:28:19 -0400

    Added comments to both UserServices to notify people that OAuth is returning user objects without their email address while Access Token is properly returning all data.

--- src/main/java/com/synopsys/integration/jira/common/server/service/UserSearchService.java
+++ src/main/java/com/synopsys/integration/jira/common/server/service/UserSearchService.java
@@ -34,6 +34,10 @@ import com.synopsys.integration.jira.common.rest.service.JiraApiClient;
 import com.synopsys.integration.rest.HttpUrl;
 import com.synopsys.integration.rest.exception.IntegrationRestException;
 
+/*
+ * Brian Mandel: There is an existing issue with OAuth returning a User object with the email field always blank.
+ * Waiting on Atlassian support to get back to us about why this is the case.
+ */
 public class UserSearchService {
     public static final String API_PATH = "/rest/api/2/user";
 
