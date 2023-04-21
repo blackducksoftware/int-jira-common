c42cb7a21f2bc104cd998b9d1fb13333665d953c
Martin Chomicki <martinch@synopsys.com>
2023-04-21 11:49:22 -0400

    feat: Break up JiraServerRestConfig and builder to support different authentication configurations

---
+++ src/main/java/com/synopsys/integration/jira/common/server/configuration/JiraServerBearerAuthRestConfig.java
@@ -0,0 +1,48 @@
+package com.synopsys.integration.jira.common.server.configuration;
+
+import java.net.URL;
+import java.util.function.BiConsumer;
+
+import com.google.gson.Gson;
+import com.synopsys.integration.jira.common.rest.JiraHttpClient;
+import com.synopsys.integration.jira.common.rest.token.JiraTokenHttpClient;
+import com.synopsys.integration.log.IntLogger;
+import com.synopsys.integration.rest.proxy.ProxyInfo;
+import com.synopsys.integration.rest.support.AuthenticationSupport;
+
+public class JiraServerBearerAuthRestConfig extends JiraServerRestConfig {
+    private final String accessToken;
+
+    public JiraServerBearerAuthRestConfig(
+        URL jiraUrl,
+        int timeoutSeconds,
+        ProxyInfo proxyInfo,
+        boolean alwaysTrustServerCertificate,
+        Gson gson,
+        AuthenticationSupport authenticationSupport,
+        String accessToken
+    ) {
+        super(jiraUrl, timeoutSeconds, proxyInfo, alwaysTrustServerCertificate, gson, authenticationSupport);
+        this.accessToken = accessToken;
+    }
+
+    @Override
+    public JiraHttpClient createJiraHttpClient(IntLogger logger) {
+        return new JiraTokenHttpClient(
+            logger,
+            getGson(),
+            getTimeoutSeconds(),
+            isAlwaysTrustServerCertificate(),
+            getProxyInfo(),
+            getJiraUrl().toString(),
+            getAuthenticationSupport(),
+            accessToken
+        );
+    }
+
+    @Override
+    public void populateEnvironmentVariables(BiConsumer<String, String> pairsConsumer) {
+        pairsConsumer.accept(JiraServerRestConfigBuilder.URL_KEY.getKey(), getJiraUrl().toString());
+        pairsConsumer.accept(JiraServerBearerAuthRestConfigBuilder.AUTH_PERSONAL_ACCESS_TOKEN.getKey(), accessToken);
+    }
+}
---
+++ src/main/java/com/synopsys/integration/jira/common/server/configuration/JiraServerBearerAuthRestConfigBuilder.java
@@ -0,0 +1,62 @@
+package com.synopsys.integration.jira.common.server.configuration;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.synopsys.integration.builder.BuilderPropertyKey;
+import com.synopsys.integration.builder.BuilderStatus;
+
+public class JiraServerBearerAuthRestConfigBuilder extends JiraServerRestConfigBuilder<JiraServerBearerAuthRestConfigBuilder, JiraServerBearerAuthRestConfig> {
+    public static final BuilderPropertyKey AUTH_PERSONAL_ACCESS_TOKEN = new BuilderPropertyKey("JIRA_AUTH_PERSONAL_ACCESS_TOKEN");
+
+    @Override
+    protected JiraServerBearerAuthRestConfigBuilder getThis() {
+        return this;
+    }
+
+    @Override
+    public Set<BuilderPropertyKey> getAuthenticationProperties() {
+        Set<BuilderPropertyKey> propertyKeys = new HashSet<>();
+        propertyKeys.add(AUTH_PERSONAL_ACCESS_TOKEN);
+        return propertyKeys;
+    }
+
+    @Override
+    public void validateAuthenticationProperties(BuilderStatus builderStatus) {
+        if (StringUtils.isBlank(getAccessToken())) {
+            builderStatus.addErrorMessage("The Jira server user name must be specified.");
+        }
+    }
+
+    @Override
+    protected JiraServerBearerAuthRestConfig buildWithoutValidation() {
+        URL jiraUrl = null;
+        try {
+            jiraUrl = new URL(getUrl());
+        } catch (MalformedURLException e) {
+        }
+
+        return new JiraServerBearerAuthRestConfig(
+            jiraUrl,
+            getTimeoutInSeconds(),
+            getProxyInfo(),
+            isTrustCert(),
+            getGson(),
+            getAuthenticationSupport(),
+            getAccessToken()
+        );
+    }
+
+    public String getAccessToken() {
+        return getProperties().get(AUTH_PERSONAL_ACCESS_TOKEN);
+    }
+
+    public JiraServerBearerAuthRestConfigBuilder setAccessToken(String accessToken) {
+        setProperty(AUTH_PERSONAL_ACCESS_TOKEN, accessToken);
+        return this;
+    }
+}
