d73d9b70cf0f77f1644181130edebc8ca39f1a88
Martin Chomicki <martinch@synopsys.com>
2023-04-25 16:31:02 -0400

    build: Add copyright header

--- src/main/java/com/synopsys/integration/jira/common/rest/token/JiraTokenHttpClient.java
+++ src/main/java/com/synopsys/integration/jira/common/rest/token/JiraTokenHttpClient.java
@@ -1,3 +1,10 @@
+/*
+ * int-jira-common
+ *
+ * Copyright (c) 2023 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.jira.common.rest.token;
 
 import java.io.IOException;
